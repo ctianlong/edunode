6bda35589de863000176bcebc6df3c83141bda7c
ctianlong <chengtl0131@gmail.com>
2018-06-19 21:06:12 +0800

    test

--- app.js
+++ app.js
@@ -9,37 +9,41 @@ function getCseUrl(_url){
 }
 
 router.all('/api/*', function(req, res, next){
-    var proxy = process.env.HTTP_PROXY || '127.0.0.1:3001';
+    var proxy = process.env.HTTP_PROXY || '127.0.0.1:30101';
     var proxy_host = proxy.substring(0, proxy.indexOf(':'));
     var proxy_port = proxy.substring(proxy.indexOf(':') + 1);;
     var opt = {
         host: proxy_host,
-        port: "30101",
+        port: proxy_port,
         method: req.method,    //这里是发送的方法
         path: getCseUrl(req.url),  //这里是访问的路径
         headers: req.headers,
         timeout: 2000,
     };
-
     var result = '';
     var request = http.request(opt, function (response) {
-        console.log("Got response: " + response.statusCode);
-        console.log(response.headers);
+        console.log(3);
         response.on('data', function (d) {
             result += d;
         }).on('end', function () {
-            console.log(result);
+            console.log(5);
             res.writeHead(response.statusCode, response.headers);
             res.write(result);
             res.end();
         });
     }).on('error', function (e) {
+        console.log(4);
         console.log("Got error: " + e.message);
     });
+    
     if(req.body){
+        console.log(req.body);
+        console.log(1);
         request.write(JSON.stringify(req.body));
+        console.log(2);
     }
     request.end();
+    console.log(6);
 });
 app.use(router);
 app.listen(8083);
