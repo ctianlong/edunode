32c5659f530ba007b59681a3e62b9eccaa54b4d2
ctianlong <chengtl0131@gmail.com>
2018-06-20 12:46:43 +0800

    test

--- app.js
+++ app.js
@@ -1,26 +1,34 @@
-const express = require('express');
-const http = require('http');
-const bodyParser = require('body-parser');
-// const multer  = require('multer');
+/**
+ * 简单的HTTP代理服务器
+ */
+var http = require('http');
 
-const app = express();
-const router = express.Router();
-// const urlencodedParser = bodyParser.urlencoded({ extended: false });
-const jsonParser = bodyParser.json();
+// 记录日志
+var log = function () {
+  var now = new Date().toISOString();
+  arguments[0] = '[' + now + '] ' + arguments[0];
+  console.log.apply(console, arguments);
+};
 
 // 获取请求的headers，去掉host和connection
 var getHeader = function (_headers) {
-    var ret = {};
-    for (var i in _headers) {
-        // 请求体转换过程中可能出现长度不一致，故把content-length也去掉
-        if (!/host|connection|content-length/i.test(i)) {
-        ret[i] = _headers[i];
-        }
+  var ret = {};
+  for (var i in _headers) {
+    if (!/host|connection/i.test(i)) {
+      ret[i] = _headers[i];
     }
-    return ret;
+  }
+  return ret;
 };
 
+// 获取请求的路径
 function getCseUrl(_url){
+    // if (_url.substr(0, 7).toLowerCase() === 'http://') {
+    //     var i = _url.indexOf('/', 7);
+    //     if (i !== -1) {
+    //         _url = _url.substr(i);
+    //     }
+    // }
     return _url.replace(/^\/api\//i, 'http://');
 }
 
@@ -28,32 +36,39 @@ var proxy = process.env.HTTP_PROXY || '127.0.0.1:30101';
 var proxy_host = proxy.substring(0, proxy.indexOf(':'));
 var proxy_port = proxy.substring(proxy.indexOf(':') + 1);
 
-router.all('/api/*', jsonParser, function(req, res, next){
-    var opt = {
-        host: proxy_host,
-        port: proxy_port,
-        method: req.method,    //这里是发送的方法
-        path: getCseUrl(req.url),  //这里是访问的路径
-        headers: getHeader(req.headers)
-    };
-    var result = '';
-    var request = http.request(opt, function (response) {
-        response.on('data', function (d) {
-            result += d;
-        }).on('end', function () {
-            res.writeHead(response.statusCode, response.headers);
-            res.write(result);
-            res.end();
-        });
-    }).on('error', function (e) {
-        console.log("Got error: " + e.stack);
-        res.writeHead("500");
-        res.end();
+// 代理请求
+var counter = 0;
+var onProxy = function (req, res) {
+  counter++;
+  var num = counter;
+  var opt = {
+    host: proxy_host,
+    port: proxy_port,
+    path:     getCseUrl(req.url),
+    method:   req.method,
+    headers:  getHeader(req.headers)
+  };
+  log('#%d\t%s http://%s%s', num, req.method, opt.host, opt.path);
+  var req2 = http.request(opt, function (res2) {
+    res.writeHead(res2.statusCode, res2.headers);
+    res2.pipe(res);
+    res2.on('end', function () {
+      log('#%d\tEND', num);
     });
-    request.write(JSON.stringify(req.body));
-    request.end();
-});
+  });
+  if (/POST|PUT|DELETE|PATCH/i.test(req.method)) {
+    req.pipe(req2);
+  } else {
+    req2.end();
+  }
+  req2.on('error', function (err) {
+    log('#%d\tERROR: %s', num, err.stack);
+    res.writeHead("500");
+    res.end();
+  });
+};
 
-app.use(router);
-app.listen(8083);
-console.log('server is start on ' + 8083);
+// 启动http服务器
+var server = http.createServer(onProxy);
+server.listen(8083);
+log('proxy server listen on http://127.0.0.1:8083');
---
+++ app1.js
@@ -0,0 +1,59 @@
+const express = require('express');
+const http = require('http');
+const bodyParser = require('body-parser');
+// const multer  = require('multer');
+
+const app = express();
+const router = express.Router();
+// const urlencodedParser = bodyParser.urlencoded({ extended: false });
+const jsonParser = bodyParser.json();
+
+// 获取请求的headers，去掉host和connection
+var getHeader = function (_headers) {
+    var ret = {};
+    for (var i in _headers) {
+        // 请求体转换过程中可能出现长度不一致，故把content-length也去掉
+        if (!/host|connection|content-length/i.test(i)) {
+        ret[i] = _headers[i];
+        }
+    }
+    return ret;
+};
+
+function getCseUrl(_url){
+    return _url.replace(/^\/api\//i, 'http://');
+}
+
+var proxy = process.env.HTTP_PROXY || '127.0.0.1:30101';
+var proxy_host = proxy.substring(0, proxy.indexOf(':'));
+var proxy_port = proxy.substring(proxy.indexOf(':') + 1);
+
+router.all('/api/*', jsonParser, function(req, res, next){
+    var opt = {
+        host: proxy_host,
+        port: proxy_port,
+        method: req.method,    //这里是发送的方法
+        path: getCseUrl(req.url),  //这里是访问的路径
+        headers: getHeader(req.headers)
+    };
+    var result = '';
+    var request = http.request(opt, function (response) {
+        response.on('data', function (d) {
+            result += d;
+        }).on('end', function () {
+            res.writeHead(response.statusCode, response.headers);
+            res.write(result);
+            res.end();
+        });
+    }).on('error', function (e) {
+        console.log("Got error: " + e.stack);
+        res.writeHead("500");
+        res.end();
+    });
+    request.write(JSON.stringify(req.body));
+    request.end();
+});
+
+app.use(router);
+app.listen(8083);
+console.log('server is start on ' + 8083);
