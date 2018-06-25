88df277fc87408f1c016812c4f2f0a8f3f5ec7ac
ctianlong <chengtl0131@gmail.com>
2018-06-25 16:52:42 +0800

    fix

--- app.js
+++ app.js
@@ -6,6 +6,10 @@ const http = require('http');
 const app = express();
 const router = express.Router();
 
+var proxy_host = process.env.HTTP_PROXY_HOST || '127.0.0.1';
+var proxy_port = process.env.HTTP_PROXY_PORT || '30101';
+var express_port = process.env.EXPRESS_PORT || 8083;
+
 // 记录日志
 var log = function () {
     var now = new Date().toLocaleString();
@@ -18,22 +22,26 @@ var getHeader = function (_headers) {
     var ret = {};
     for (var i in _headers) {
         if (!/host|connection/i.test(i)) {
-        ret[i] = _headers[i];
+            ret[i] = _headers[i];
         }
     }
     return ret;
 };
 
 // 转换URL
-function getCseUrl(_url){
+function getCseUrl(_url) {
     return _url.replace(/^\/api\//i, 'http://');
 }
 
-var proxy_host = process.env.HTTP_PROXY_HOST || '127.0.0.1';
-var proxy_port = process.env.HTTP_PROXY_PORT || '30101';
-
 var counter = 0;
-router.all('/api/*', function(req, res, next){
+router.all('/api/*', function (req, res, next) {
+    // 解决跨域
+    if (/OPTIONS/i.test(req.method)) {
+        res.writeHead(200, { 'Access-Control-Allow-Origin': '*', 'Access-Control-Allow-Methods': 'GET,POST,PUT,DELETE,PATCH', 'Access-Control-Max-Age': 1728000 });
+        res.end();
+        return;
+    }
+
     counter++;
     var num = counter;
     var opt = {
@@ -46,6 +54,7 @@ router.all('/api/*', function(req, res, next){
     log('#%d\t%s %s %s %s', num, opt.method, opt.host, opt.port, opt.path);
     var req2 = http.request(opt, function (res2) {
         headers = res2.headers;
+        // 解决跨域
         headers['Access-Control-Allow-Origin'] = '*';
         res.writeHead(res2.statusCode, headers);
         res2.pipe(res);
@@ -55,7 +64,7 @@ router.all('/api/*', function(req, res, next){
     });
     req2.on('error', function (e) {
         log('#%d\tERROR: %s', num, e.stack);
-        res.writeHead("500");
+        res.writeHead("502");
         res.end();
     });
     if (/POST|PUT|DELETE|PATCH/i.test(req.method)) {
@@ -65,6 +74,14 @@ router.all('/api/*', function(req, res, next){
     }
 });
 
+// api接口
 app.use(router);
-app.listen(8083);
-log('proxy server listen on 8083');
+// 静态资源
+app.use(express.static(__dirname + '/dist'));
+// 所有虚拟路由转向vue页面
+app.use(function (req, res) {
+    res.sendFile(__dirname + "/dist/index.html");
+});
+// 启动端口监听
+app.listen(express_port);
+log('proxy server listen on ' + express_port);
